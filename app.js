7871c9448085c811437df7d4dbc4936a7c769a6d
svphan29 <36240410+svphan29@users.noreply.github.com>
2018-10-23 09:57:45 -0600

    on posting

--- app.js
+++ app.js
@@ -1,2 +1,33 @@
 const express = require('express');
-const cakes = require('data.json');
+const app = express();
+const cakes = require('./data.json');
+const bodyParser = require('body-parser');
+const port = 3100;
+
+app.get('/', (req, res, next) => {
+  res.send('rawr')
+});
+
+app.get('/cakes', (req, res, next) => {
+  res.json({ cakes: cakes })
+})
+
+app.get('/cakes/:id', (req, res, next) => {
+  const id = req.params.id
+  const cake = cakes.filter(cake => {
+    return cake.id == id
+  })
+  res.json({cake})
+})
+
+app.use(bodyParser.json());
+
+app.post('/cakes', (req, res, next) => {
+  const body = req.body
+  characters.push(body)
+  res.json({ body: body })
+})
+
+app.listen(port, () => console.log(`I got you on ${port}`));
+
+
